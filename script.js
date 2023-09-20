7be8c28cbfebf3470b7fdbb157424ec9dd4e5e3c
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2023-09-20 10:53:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // Your code here.
+let isDragging = false;
+let initialX;
+let cubes = document.querySelectorAll('.item');
+
+cubes.forEach(cube => {
+  cube.addEventListener('mousedown', (e) => {
+    isDragging = true;
+    initialX = e.clientX;
+    cube.style.cursor = 'grabbing';
+  });
+
+  document.addEventListener('mousemove', (e) => {
+    if (!isDragging) return;
+
+    const deltaX = e.clientX - initialX;
+    cube.style.transform = `translateX(${deltaX}px)`;
+  });
+
+  document.addEventListener('mouseup', () => {
+    if (isDragging) {
+      cube.style.cursor = 'grab';
+      isDragging = false;
+      initialX = null;
+    }
+  });
+});
