209ef0889a40cf4466b1d2087b2f3a88c456fffe
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2023-09-28 10:19:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,59 @@
-// Your code here.
-let isDragging = false;
-let initialX;
-let cubes = document.querySelectorAll('.item');
+//your code here
+let draggingElement = null;
 
-cubes.forEach(cube => {
-  cube.addEventListener('mousedown', (e) => {
-    isDragging = true;
-    initialX = e.clientX;
-    cube.style.cursor = 'grabbing';
-  });
+        // Event listener for when a drag starts
+        document.addEventListener('dragstart', (event) => {
+            draggingElement = event.target;
+            event.dataTransfer.setData('text/plain', null); // Required for Firefox
+            event.target.classList.add('dragging');
+        });
 
-  document.addEventListener('mousemove', (e) => {
-    if (!isDragging) return;
+        // Event listener for when a drag ends
+        document.addEventListener('dragend', (event) => {
+            draggingElement.classList.remove('dragging');
+            draggingElement = null;
+        });
 
-    const deltaX = e.clientX - initialX;
-    cube.style.transform = `translateX(${deltaX}px)`;
-  });
+        // Event listener for when a dragged item enters a drop target
+        document.addEventListener('dragenter', (event) => {
+            if (event.target.classList.contains('item')) {
+                event.target.style.border = '2px dashed #000';
+            }
+        });
 
-  document.addEventListener('mouseup', () => {
-    if (isDragging) {
-      cube.style.cursor = 'grab';
-      isDragging = false;
-      initialX = null;
-    }
-  });
-});
+        // Event listener for when a dragged item leaves a drop target
+        document.addEventListener('dragleave', (event) => {
+            if (event.target.classList.contains('item')) {
+                event.target.style.border = '';
+            }
+        });
+
+        // Event listener for when a dragged item is over a drop target
+        document.addEventListener('dragover', (event) => {
+            event.preventDefault(); // Allow drop
+        });
+
+        // Event listener for when a dragged item is dropped onto a drop target
+        document.addEventListener('drop', (event) => {
+            event.preventDefault();
+            if (event.target.classList.contains('item')) {
+                event.target.style.border = '';
+                swapImages(draggingElement, event.target);
+            }
+        });
+
+        // Function to swap the positions of two images
+        function swapImages(image1, image2) {
+            const container = document.getElementById('items');
+            const images = Array.from(container.children);
+            const index1 = images.indexOf(image1);
+            const index2 = images.indexOf(image2);
+
+            if (index1 !== -1 && index2 !== -1) {
+                // Swap the images in the array
+                [images[index1], images[index2]] = [images[index2], images[index1]];
+
+                // Update the DOM to reflect the new order
+                images.forEach((image) => container.appendChild(image));
+            }
+        }
